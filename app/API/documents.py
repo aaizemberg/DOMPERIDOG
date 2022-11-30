21ef4ab9ab7796e39bd8ed3b0f3a020e8c2d844d
federojas <frojas@itba.edu.ar>
2022-11-29 21:10:32 -0300

    dax try 2

--- app/API/documents.py
+++ app/API/documents.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
 from app.core.schemas.document import Document
 from app.core.models.document_data import DocumentData
 from app.dbs import document_collection
+from app.API.users import get_current_user
+from app.core.schemas.user import User
+from datetime import datetime
 
 router = APIRouter()
 
@@ -11,11 +14,16 @@ router = APIRouter()
         response_model = Document
     )
 async def create_document(
-        document: DocumentData
+        document: DocumentData,
+        current_user: User = Depends(get_current_user)
     ):
     new_document = {
         "title": document.title,
-        "content": document.content
+        "content": document.content,
+        "author": current_user,
+        "editors": [],
+        "public": True,
+        "creation_date": datetime.now()
     }
 
     document_collection.insert_one(new_document)
