daa20953cc7410103eaeacdf150092995d78cbf8
federojas <frojas@itba.edu.ar>
2022-11-30 10:16:19 -0300

    more functions

--- app/API/documents.py
+++ app/API/documents.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from app.core.schemas.document import Document
 from app.core.models.document_data import DocumentData
-from app.dbs import document_collection
+from app.dbs import document_collection, user_collection
 from app.API.users import get_current_user
 from app.core.schemas.user import User
 from datetime import datetime
@@ -9,6 +9,7 @@ from bson.objectid import ObjectId
 from pymongo import ReturnDocument
 from typing import List
 import re
+from app.core.models.editor_data import EditorData
 
 router = APIRouter()
 
@@ -174,7 +175,7 @@ async def change_document_visibility_by_id(
     status_code = status.HTTP_200_OK,
     response_model = List[Document]
 )
-async def get_document_by_id(
+async def search_document(
         search: str,
         current_user: User = Depends(get_current_user)
     ):
@@ -202,3 +203,54 @@ async def get_document_by_id(
         raise not_found_exception
 
     return get_documents
+
+
+@router.put(
+"/{document_id}/editors", 
+status_code = status.HTTP_200_OK,
+response_model = Document
+)
+async def change_document_editor_by_username(
+        document_id: str,
+        editor_username: str,
+        current_user: User = Depends(get_current_user)
+    ):
+
+    document_not_found_exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Specified document not found"
+    )
+
+    user_not_found_exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Specified user not found"
+    )
+
+    forbidden_exception = HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="You are not authorized to change the editors of this document"
+    )
+
+    editor_is_author_exception = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="The author can not be an editor"
+    )
+
+
+    edit_document = document_collection.find_one({"_id": ObjectId(document_id)})
+    if edit_document is None:
+        raise document_not_found_exception
+    if not edit_document["author"] == current_user:
+        raise forbidden_exception  
+
+    subject_editor = user_collection.find_one({"username": editor_username})
+    if subject_editor is None:
+        raise user_not_found_exception
+        
+    if subject_editor in edit_document["editors"]:
+        return_document = document_collection.find_one_and_update({"_id": ObjectId(document_id)}, { '$pull': { "editors" :  subject_editor} },  return_document = ReturnDocument.AFTER)
+    elif not subject_editor == edit_document["author"]:
+        return_document = document_collection.find_one_and_update({"_id": ObjectId(document_id)}, { '$push': { "editors" :  subject_editor} },  return_document = ReturnDocument.AFTER)
+    else:
+        raise editor_is_author_exception
+    return return_document
