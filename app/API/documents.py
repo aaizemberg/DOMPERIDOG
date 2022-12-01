338166a2a82612978e7692f521e04997e8f0bc2a
federojas <frojas@itba.edu.ar>
2022-12-01 13:02:25 -0300

    possible fix

--- app/API/documents.py
+++ app/API/documents.py
@@ -72,8 +72,6 @@ async def search_document(
     }
 
     get_documents = document_collection.find(search_request).sort("creation_date", -1).skip((page - 1) * page_size).limit(page_size)
-    if get_documents is None:
-        raise not_found_exception
     
     document_list = []
     for doc in get_documents:
@@ -81,7 +79,7 @@ async def search_document(
 
     return PaginatedDocument(
         current_page = page,
-        total_pages = len(document_list) // page_size + 1,
+        total_pages = document_collection.count_documents({search_request}) // page_size + 1,
         page_size = page_size,
         documents = document_list
     ) #TODO devolver los documentos
